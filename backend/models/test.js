5f015877c7953ae524d16f4bbced98f1705e2e22
Shubham Kumar Soni <shubhamkumar99399006@gmail.com>
2025-10-27 01:20:06 +0530

    Remove backend folder

--- backend/models/test.js
+++
@@ -1,6 +0,0 @@
-const crypto = require('crypto');
-
-// Generate a secure random key
-const secretKey = crypto.randomBytes(32).toString('hex');
-
-console.log(secretKey);
--- backend/utils/timeUtils.js
+++
@@ -1,8 +0,0 @@
-const convertToIST = (date) => {
-    if (!date || !(date instanceof Date) || isNaN(date)) {
-        return new Date(); // Return current time if invalid date
-    }
-    return new Date(date.getTime() + (5.5 * 60 * 60 * 1000)); // Add 5:30 hours for IST
-};
-
-module.exports = { convertToIST };
